5a8da22d5478f4da541332c149cfce9437492cc7
Admin <admin@Admins-MacBook-Air.local>
2023-01-23 14:24:43 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import News from '/Users/admin/React_news/newsapp/src/NewsApp/News.jsx'
+// import HeadlineFun from  '/Users/admin/React_news/newsapp/src/NewsApp/HeadlineFun.jsx'
 function App() {
   return (
     <>
     <News/>
+    {/* <HeadlineFun/> */}
     </>
   );
 }
