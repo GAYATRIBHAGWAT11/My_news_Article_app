3a5e0b5eb681c0843dea17869c7909d3e946eb41
Admin <admin@Admins-MacBook-Air.local>
2023-01-23 13:00:03 +0530

    some changes in news article app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import News from '/Users/admin/React_news/newsapp/src/NewsApp/News.jsx'
+function App() {
+  return (
+    <>
+    <News/>
+    </>
+  );
+}
+
+export default App;
